7f1b77ccde73203742dba69c0da355d0555ba019
hasanmd91 <hasanmd1991@hotmail.com>
2024-01-31 23:16:32 +0200

    feat: Add DataBaseContext for managing entity data access
    - Implement a DbContext for managing database interactions
    - Define DbSet properties for Station and Journey entities
    - Configure PostgreSQL-specific options for naming convention and warnings

--- CityBike.WebApi/Program.cs
+++ CityBike.WebApi/Program.cs
@@ -1,9 +1,27 @@
+using CityBike.WebApi.src.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "CityBike", Version = "v1" });
+    });
+
+
+
+var dataSourceBuilder = new NpgsqlDataSourceBuilder(builder.Configuration.GetConnectionString("SolitaDB"));
+var dataSource = dataSourceBuilder.Build();
+builder.Services.AddDbContext<DataBaseContext>(options => options.UseNpgsql(dataSource));
+
+
 
 var app = builder.Build();
 
---
+++ CityBike.WebApi/src/Database/DataBaseContext.cs
@@ -0,0 +1,38 @@
+using CityBike.Core.src.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Npgsql;
+
+namespace CityBike.WebApi.src.Database
+{
+    public class DataBaseContext : DbContext
+    {
+        public DbSet<Station> Stations { get; set; }
+        public DbSet<Journey> Journeys { get; set; }
+
+
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder
+            .UseSnakeCaseNamingConvention()
+            .ConfigureWarnings(warnings => { warnings.Ignore(CoreEventId.ManyServiceProvidersCreatedWarning); });
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+
+
+
+}
