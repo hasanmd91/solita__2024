0371a94ef8a394928dd34c025a5dddff6a33948a
hasanmd91 <hasanmd1991@hotmail.com>
2024-01-31 23:14:53 +0200

    feat: Add IBaseRepo interface
    - Define IBaseRepo interface in Abstraction namespace
    - Includes methods for CRUD operations and retrieving data

---
+++ CityBike.Core/src/Abstraction/IBaseRepo .cs
@@ -0,0 +1,15 @@
+using CityBike.Core.src.Shared;
+
+namespace CityBike.Core.src.Abstraction
+{
+    public interface IBaseRepo<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync(GetAllOptions options);
+        Task<int> GetTotal(GetAllOptions options);
+        Task<T?> GetByIdAsync(Guid id);
+        Task<T> CreateOneAsync(T createObject);
+        Task<T> UpdateOneAsync(T updateObject);
+        Task<bool> DeleteOneAsync(T deleteObject);
+
+    }
+}
